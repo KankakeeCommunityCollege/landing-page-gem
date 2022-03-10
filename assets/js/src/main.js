b6b474c60c36338dc8ea8544f3a762b8db9f226a
wdzajicek <tig.welder4@gmail.com>
2022-03-10 13:25:41 -0600

    Update MFS logo for 2022-23. Move JS into window.onload event.

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -2,25 +2,29 @@ import '../../scss/main.scss';
 // import test from './test';
 
 function loadModule(module) {
-  import(`./${module}`).then(({default: module}) => {
-    module();
-  });
+  return import(`./${module}`).then(({default: module}) => module())
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
 
   Promise.resolve()
     .then(() => {
-      if ( window.location.hostname.search(/\.kcc\.edu/) !== -1 ) {
-        loadModule('loadClarusCorpScript');
-    }})
+      if (window.location.hostname.search(/\.kcc\.edu/) == -1)
+        return;
+      
+      return loadModule('loadClarusCorpScript');
+    }).then(() => {
+      if (!document.querySelector('img[data-src]'))
+        return;
+      
+      return loadModule('lazyLoad');
+    }).then(() => loadModule('footerDate'))
     .then(() => {
-    if ( document.querySelector('img[data-src]') ) {
-      loadModule('lazyLoad');
-    }})
-    .then(() => loadModule('footerDate'))
-    .then(() => {
-      if ( document.querySelector('a[href="#page-top"]') ) {
-        loadModule('landingPage');
-    }})
+      if (!document.querySelector('a[href="#page-top"]'))
+        return;
+      
+      return loadModule('landingPage');
+    }).catch(err => {
+      console.error(`Error loading landing-page JS modules: ${err.message}`, err);
+    })
 });
