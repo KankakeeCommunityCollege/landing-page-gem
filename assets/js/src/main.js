9e2f3d77fa92b41e5491fe55774dc0d2dbccfc50
wdzajicek <tig.welder4@gmail.com>
2026-09-15 16:33:47 -0500

    Add JS to build accessible external links.

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,6 +1,6 @@
 import '../../scss/main.scss';
 
-window.addEventListener('load', async () => {
+document.addEventListener('DOMContentLoaded', async () => {
   if (document.querySelector('img[data-src]')) {
     const { default: lazyLoad } = await import('./lazyLoad');
 
@@ -14,6 +14,12 @@ window.addEventListener('load', async () => {
     const { default: landingPage } = await import('./landingPage');
 
     landingPage();
+  }
+
+  if (document.querySelector('main a')) {
+    import('./accessibleExternalLinks')
+      .then(({ default: accessibleExternalLinks }) => accessibleExternalLinks())
+      .catch(err => console.error('Error importing "accessibleExternalLinks" module: ', err));
   }
 
   if (document.getElementById('SearchTermForm')) {
